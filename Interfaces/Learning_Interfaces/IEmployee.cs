b0e3f1d371b84e3e00b3d71dce6bfb8a78188da5
Aamir Ali <aa882755@gmail.com>
2024-09-15 13:08:34 +0530

    Interface

---
+++ Interfaces/Learning_Interfaces/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿namespace Learning_Interfaces;
+
+public interface IEmployee
+{
+    int Id { get; set; }
+    string? JobTitle { get; set; }
+    string? FirstName { get; set; }
+    string? LastName { get; set; }
+    decimal? AnnualSalary { get; set; }
+    char? Gender { get; set; }
+    DateTime? JoinDate { get; set; }
+    string? HighestQualification { get; set; }
+    string GetBasicInformation();
+}
